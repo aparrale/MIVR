21eac84da4788cc9a59de4cff33c7d78ea47dd28
aparrale <aparrale@ucsc.edu>
2022-10-04 20:23:46 -0700

    added trigger
    Truck has a trigger in the bed that will update the UI above

---
+++ MIVR_Prototype/Assets/capacityScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class capacityScript : MonoBehaviour 
+{
+    public Image capacityBar; // iniitialize public var
+
+    void Start() 
+    {
+        setFill(0);
+    }
+
+    public void setFill(float amount) //set fill amount to 0 
+    {
+        capacityBar.fillAmount = amount;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+     Debug.Log("Trigger");   
+    }
+
+}

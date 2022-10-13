b4eeeb660e9790c56574532bdc30f93b164f8f33
aparrale <aparrale@ucsc.edu>
2022-10-12 21:23:29 -0700

    Update capacityScript.cs

--- MIVR_Prototype/Assets/capacityScript.cs
+++ MIVR_Prototype/Assets/capacityScript.cs
@@ -6,30 +6,25 @@ using UnityEngine.UI;
 public class capacityScript : MonoBehaviour 
 {
     public Image capacityBar; // iniitialize public var
-    private ItemWeight itemWeight; //initialize class script
+    private ItemWeight itemWeight; // initialize variable of type script
 
-    void setFill(float amount) //set fill amount to 0 
+    void setFill(float amount) // function to set fill amount
     {
         capacityBar.fillAmount = amount;
     }
 
-    void Start() 
+    void Start() // set fill amount to 0 upon game start
     {
         setFill(0);
     }
 
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other) // function upon collision, add fill to the UI bar
     {
-        itemWeight = other.GetComponent<ItemWeight>();
-     Debug.Log("Trigger" + itemWeight.Mass);
+        itemWeight = other.GetComponent<ItemWeight>(); // retrieve component from other script and store in var itemweight
+     Debug.Log("Trigger" + itemWeight.Mass); // debug log to make sure mass is being triggered
             
-         capacityBar.fillAmount += itemWeight.Mass;
+         capacityBar.fillAmount += itemWeight.Mass; // fill the bar by the amount of mass
     }
-
-    //private void AddWeight (float amount)
-    //{
-        //capacityBar.fillAmount += itmeWeight.Mass;
-    //}
 
 }
